252d82b234838577efbc38aea4a05b482e90fcc6
fengshuonan <sn93@qq.com>
2022-09-07 13:46:25 +0800

    【7.2.5】【system】将format类放到api包下

--- kernel-s-system/system-api/src/main/java/cn/stylefeng/roses/kernel/system/api/format/UserFormatProcess.java
+++ kernel-s-system/system-api/src/main/java/cn/stylefeng/roses/kernel/system/api/format/UserFormatProcess.java
@@ -1,4 +1,4 @@
-package cn.stylefeng.roses.kernel.system.modular.user.format;
+package cn.stylefeng.roses.kernel.system.api.format;
 
 import cn.hutool.core.convert.Convert;
 import cn.hutool.extra.spring.SpringUtil;
