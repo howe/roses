9cb9fd961d419e1c66e3286aaefeac81ff9c25bb
fengshuonan <sn93@qq.com>
2022-11-01 21:13:34 +0800

    【7.3.2】修复租户判空问题

--- kernel-d-validator/validator-api-table-unique/src/main/java/cn/stylefeng/roses/kernel/validator/api/validators/unique/service/TableUniqueValueService.java
+++ kernel-d-validator/validator-api-table-unique/src/main/java/cn/stylefeng/roses/kernel/validator/api/validators/unique/service/TableUniqueValueService.java
@@ -164,7 +164,7 @@ public class TableUniqueValueService {
         String tenantCode = loginUser.getTenantCode();
 
         // 如果是主数据源可以忽略
-        if (tenantCode == null || TenantConstants.MASTER_DATASOURCE_NAME.equals(tenantCode)) {
+        if (ObjectUtil.isNotEmpty(tenantCode) || TenantConstants.MASTER_DATASOURCE_NAME.equals(tenantCode)) {
             return originTableName;
         }
 
